68faaf8274635f677e165cde5f529a9888563c4c
Your Name <you@example.com>
2020-02-09 17:43:26 -0300

    Implementando login e cadastro de usuario

---
+++ database/login.sql
@@ -0,0 +1,5 @@
+create table login (
+    id integer primary key autoincrement,
+    login varchar(50) not null,
+    senha varchar(100) not null
+);
---
+++ login.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+BANCO='database/agenda.db'
+TABELA='login'
+source lib_db
+encrypt(){
+    senha=$(echo "$@" | shasum | cut -d' ' -f1) 
+}
+
+cor(){
+    vermelho='\033[41m'
+    verde='\033[42m'
+    amarelo='\033[43;30m'
+    azul='\033[44m'
+    fim='\033[m'
+}
+
+login(){
+    cor
+    local linha=10
+    local coluna=40
+    local posicao="\033c\033[$linha;$coluna"
+    for ((i = 0; i < 3; i++ )); do
+        printf "$azul $posicao"'H Login:  ' "$fim"
+        read login
+        stty -echo
+        printf "$azul $posicao"'H Senha:  ' "$fim"
+        read senha
+        encrypt $senha
+        stty echo 
+        echo -e "$fim"
+        [ ! $( _find "where login='$login' and senha='$senha'" ) ] || {
+            echo
+            echo -e "$verde Ola '$login' sua senha eh '$senha' $fim"
+            return
+        }
+            echo -e  "$vermelho Acesso negado! $fim"
+    done
+    exit 1
+}
+
+usuario(){
+    cor
+    local login senha
+    echo "Cadastrar usuario"
+    echo -ne "$azul login: $fim"
+    read login
+    echo -e "$azul senha: $fim"
+    stty -echo
+    read senha
+    echo -ne "$amarelo Confirme a senha: $fim "
+    read confirme
+    stty echo
+    echo
+    [ "$senha" == "$confirme"   ] || {
+        echo -e "$vermelho Senha incorreta $fim "
+        return 1
+    }
+
+    [[ "$login" && "$senha"  ]] || {
+        echo -e  "$vermelho Login ou senha esta vazio $fim"
+        return 1
+    }
+    encrypt $senha
+    _max
+   #_insert $max $login $senha
+}
+login
