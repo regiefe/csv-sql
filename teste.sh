902f5009b288770f1a3995432a7a4edd74712f18
Your Name <you@example.com>
2020-02-11 18:01:11 -0300

    Cadastro de contatos

--- teste.sh
+++ teste.sh
@@ -2,16 +2,16 @@
 BANCO='database/agenda.db'
 TABELA='usuario'
 source lib_db
+_find outro
 _all
 echo
 _all login
-echo
 _insert outro 'isso+aqui+é+um+teste' 23 M
 echo
-_find outro
 echo
 _update outro 'Isso+vou+alterado' 23 M
 echo
 _find outro
 echo
 _delete outro
+
