de1debcfba8d701091f442e1a201bf76d28261ce
Your Name <you@example.com>
2020-02-19 17:53:50 -0300

    Criando relacionamento entre tabelas

--- converte-csv.sh
+++ converte-csv.sh
@@ -4,18 +4,39 @@ BANCO='database/banco.db'
 arquivo=$1
 tabela=${arquivo%.csv}
 
-echo "$(sed -n 1p $arquivo | sed  "s/;/ text, /g; s/.*/CREATE TABLE $tabela ( & text );/")" | sqlite3 "$BANCO"
-echo .schema "$tabela" | sqlite3 "$BANCO"
+cria_campos(){
+    campos=$(sed -n 1p $arquivo | sed 's/;/, /g' )
+    echo "$campos" | sed "s/, / text, /g;s/.*/CREATE TABLE $tabela ( & text  );/" | sqlite3 "$BANCO"
+    echo .schema "$tabela" | sqlite3 "$BANCO" >> log
+}
 
-arquivo=$(sed 1d $arquivo)
-mkdir temp
-echo $arquivo > temp/file
-cat temp/file | sed 's/ /\n/g' > temp/newfile
-echo ".import temp/newfile $tabela" | sqlite3 -separator ";" $BANCO
-rm -r temp
-sqlite3 $BANCO "select * from $tabela"
-echo "CREATE TABLE new$tabela ( id integer primary key autoincrement, login text, nome text, idade text, sexo text )" | sqlite3 $BANCO
-echo "INSERT INTO new$tabela (login, nome, idade, sexo) SELECT * FROM $tabela" | sqlite3 $BANCO
-echo "DROP TABLE $tabela" | sqlite3 $BANCO
-echo "ALTER TABLE new$tabela RENAME TO $tabela" | sqlite3 $BANCO
-sqlite3 $BANCO "select * from $tabela"
+cria_tabela(){
+    arquivo=$(sed 1d $arquivo)
+    mkdir temp
+    echo $arquivo > temp/file
+    cat temp/file | sed 's/ /\n/g' > temp/newfile
+    echo ".import temp/newfile $tabela" | sqlite3 -separator ";" $BANCO
+    rm -r temp
+    sqlite3 $BANCO "select * from $tabela" >> log
+}
+
+criar_indice(){
+    echo "CREATE TABLE new$tabela ( id integer primary key autoincrement, $campos )" | sqlite3 $BANCO
+    echo "INSERT INTO new$tabela ( $campos ) SELECT * FROM $tabela" | sqlite3 $BANCO
+    echo "DROP TABLE $tabela" | sqlite3 $BANCO
+    echo "ALTER TABLE new$tabela RENAME TO $tabela" | sqlite3 $BANCO
+    echo "SELECT * FROM $tabela" | sqlite3 $BANCO >> log
+}
+
+cria_tabela_indice(){
+    tabelas=($(sqlite3 $BANCO .tables))
+
+    local i
+    for ((i=0; i<${#tabelas[@]}; i++)) 
+    do
+        ${tabelas[$i]}_id=$(echo "select id from ${tabelas[$i]}" | sqlite3 database/banco.db) 
+    done
+}
+cria_campos
+cria_tabela
+criar_indice
