b830c9fed2f7ca07414db5565fa8531d96340975
Reginaldo F.S <regiefe@gmail.com>
2020-03-06 16:58:53 -0300

    Relacionamento implementado falta ajuste

--- converte-csv.sh
+++ converte-csv.sh
@@ -11,7 +11,7 @@ cria_campos(){
     [ -d log ] || {
         mkdir log
     }
-    echo "$(date +'%Y-%m-%d H%:m%') : Criado os campo da tabela $tabela" > log/$(echo $tabela)-log
+    echo "$(date +'%Y-%m-%d H%:%M') : Criado os campo da tabela $tabela" > log/$(echo $tabela)-log
     sqlite3 "$BANCO" ".schema $tabela" >> log/$(echo $tabela)-log
     echo >> log/$(echo $tabela)-log
 }
@@ -20,7 +20,7 @@ cria_tabela(){
     cria_campos
     sed 1d $arquivo > $temp
     echo ".import $temp $tabela" | sqlite3 -separator ";" $BANCO
-    echo "$(date +'%Y-%m-%d H%:m%') : Gerado a  tabela $tabela" >> log/$(echo $tabela)-log
+    echo "$(date +'%Y-%m-%d H%:%M') : Gerado a  tabela $tabela" >> log/$(echo $tabela)-log
     sqlite3 $BANCO "SELECT * FROM $tabela" >> log/$(echo $tabela)-log
     echo >> log/$(echo $tabela)-log
 }
@@ -31,37 +31,10 @@ cria_indice(){
     echo "INSERT INTO new$tabela ( $campos ) SELECT * FROM $tabela" | sqlite3 $BANCO
     echo "DROP TABLE $tabela" | sqlite3 $BANCO
     echo "ALTER TABLE new$tabela RENAME TO $tabela" | sqlite3 $BANCO
-    echo "$(date +'%Y-%m-%d H%:m%') : Gerado o indice da tabela $tabela" >> log/$(echo $tabela)-log
+    echo "$(date +'%Y-%m-%d H%:%M') : Gerado o indice da tabela $tabela" >> log/$(echo $tabela)-log
     sqlite3 $BANCO -header -column "SELECT * FROM $tabela" >> log/$(echo $tabela)-log
     echo >> log/$(echo $tabela)-log
 }
 
 cria_indice
 
-relaciona(){
-    tabelas=($(sqlite3 $BANCO ".table"))
-    echo  ${#tabelas[@]}
-: '
-    PRAGMA FOREIGN_KEYS=on;
-    CREATE TABLE relacao ( 
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        cargos_id INTEGER, 
-        pessoa_id INTEGER, 
-        FOREIGN KEY (cargos_id) REFERENCES cargos(id), 
-        FOREIGN KEY (pessoa_id) REFERENCES pessoa(id) 
-    );
-    INSERT INTO relacao  VALUES (null, 1,1 );
-    INSERT INTO relacao  VALUES (null, 1,7 );
-    INSERT INTO relacao  VALUES (null, 5,2 );
-    INSERT INTO relacao  VALUES (null, 2,3 );
-    INSERT INTO relacao  VALUES (null, 3,5 );
-    INSERT INTO relacao  VALUES (null, 7,3 );
-
-    SELECT p.nome contratado , c.cargo 
-        FROM pessoa p  
-        LEFT JOIN relacao r ON p.id=r.pessoa_id  
-        JOIN cargos c ON c.id=r.cargos_id;
-    SELECT * FROM pessoa p LEFT JOIN cargos c  ON p.id=c.id ;
-'
-}
-relaciona
